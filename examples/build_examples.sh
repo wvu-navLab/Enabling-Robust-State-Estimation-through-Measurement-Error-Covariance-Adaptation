b6e986ca5e82c28334351e7afc276b8eb0944b3b
watsonryan <rwatso12@gmail.com>
2019-05-01 16:30:34 -0400

    now properly linking to libs

--- examples/build_examples.sh
+++ examples/build_examples.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+
 CURRDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd && cd .. )"
 DIR="$(dirname "$CURRDIR")"
 
 mkdir -p build
 
+LDIR="$DIR/lib"
+IDIR="$DIR/include"
+EDIR="$DIR/3rdparty/Eigen"
+BDIR="$CURRDIR/build"
+
 #-------------------------------------------------------------------------------------------------
 # Build example files
 #--------------------------------------------------------------------------------------------------
 
-g++ test_gnss_bce.cpp -std=c++11 -lboost_system -lboost_program_options -ltbb -I"$DIR/include/"  -L"$DIR/lib/" -lcluster -lgpstk -Wno-deprecated-declarations -lgtsam -I"$DIR/3rdparty/Eigen/" -fopenmp -o "$DIR/examples/build/test_gnss_bce"
+g++ test_gnss_ncad_res.cpp -std=c++11 -I"$EDIR" -L"$LDIR" -Wl,-rpath="$LDIR" -I"$IDIR" -lboost_system -lboost_program_options -ltbb -lcluster -Wno-deprecated-declarations -lgtsam -fopenmp -o "$BDIR/test_gnss_ncad_res"
 
-g++ test_gnss_maxmix.cpp -std=c++11 -lboost_system -lboost_program_options -ltbb -I"$DIR/include/"  -L"$DIR/lib/" -lgpstk -Wno-deprecated-declarations -lgtsam -I"$DIR/3rdparty/Eigen/" -o "$DIR/examples/build/test_gnss_maxmix"
+g++ test_gnss_bce.cpp -std=c++11 -I"$EDIR" -L"$LDIR" -Wl,-rpath="$LDIR" -I"$IDIR" -lboost_system -lboost_program_options -ltbb -lcluster -Wno-deprecated-declarations -lgtsam -fopenmp -o "$BDIR/test_gnss_bce"
 
-g++ test_gnss_dcs.cpp -std=c++11 -lboost_system -lboost_program_options -ltbb -I"$DIR/include/"  -L"$DIR/lib/" -lgpstk -Wno-deprecated-declarations -lgtsam -I"$DIR/3rdparty/Eigen/" -o "$DIR/examples/build/test_gnss_dcs"
+g++ test_gnss_maxmix.cpp -std=c++11 -I"$EDIR" -L"$LDIR" -Wl,-rpath="$LDIR" -I"$IDIR" -lboost_system -lboost_program_options -ltbb -Wno-deprecated-declarations -lgtsam -fopenmp -o "$BDIR/test_gnss_maxmix"
 
-g++ rnx_2_gtsam.cpp -std=c++11 -lboost_system  -lboost_program_options -I"$DIR/include/"  -L"$DIR/lib/"  -lgpstk -Wno-deprecated-declarations -o "$DIR/examples/build/rnx_2_gtsam"
+g++ test_gnss_dcs.cpp -std=c++11 -I"$EDIR" -L"$LDIR" -Wl,-rpath="$LDIR" -I"$IDIR" -lboost_system -lboost_program_options -ltbb -Wno-deprecated-declarations -lgtsam -fopenmp -o "$BDIR/test_gnss_dcs"
+
+g++ rnx_2_gtsam.cpp -std=c++11 -L"$LDIR" -Wl,-rpath="$LDIR" -I"$IDIR" -lboost_system -lboost_program_options -ltbb -Wno-deprecated-declarations -lgpstk -fopenmp -o "$BDIR/rnx_2_gtsam"
